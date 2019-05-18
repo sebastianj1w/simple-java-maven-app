2949c9ff799e416f9ca26f88cb1e3c4fc39269f4
sebastianj1w <sebastianj1w@outlook.com>
2019-05-18 15:32:58 +0800

    1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World111!";
+    private final String message = "Helqqqqlo World111!";
 
     public App() {}
 
