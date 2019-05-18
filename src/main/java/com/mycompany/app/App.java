3034980c4d1a944e0557c54c0901311007b5184e
sebastianj1w <sebastianj1w@outlook.com>
2019-05-18 21:25:26 +0800

    change test #3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World1qq11eee!";
+    private final String message = "Hello World123!";
 
     public App() {}
 
